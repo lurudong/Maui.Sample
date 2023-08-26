03528dd117579615fae340bef8b3427f2611335a
dong <44517714+lurudong@users.noreply.github.com>
2023-08-26 14:35:03 +0800

    新增My.Task项目

---
+++ src/My.Task/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace My.Task
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/My.Task/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace My.Task
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
