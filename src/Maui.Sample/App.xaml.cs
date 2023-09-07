c8230bb8c90b4cfd2f49690c524ef1e6c92111d8
dong <44517714+lurudong@users.noreply.github.com>
2023-09-07 18:48:26 +0800

    1

--- src/Maui.Sample/App.xaml.cs
+++ src/Maui.Sample/App.xaml.cs
@@ -8,5 +8,29 @@
 
             MainPage = new AppShell();
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="appAction"></param>
+        public static void HandleAppActions(AppAction appAction)
+        {
+            App.Current.Dispatcher.Dispatch(async () =>
+            {
+
+                var page = appAction.Id switch
+                {
+                    "battery_info" => new MainPage(),
+                    "app_info" => new MainPage(),
+                    _ => default(Page)
+                };
+
+                if (page != null)
+                {
+                    await Application.Current.MainPage.Navigation.PopToRootAsync();
+                    await Application.Current.MainPage.Navigation.PushAsync(page);
+                }
+            });
+        }
     }
 }
--- src/Maui.Sample/MauiProgram.cs
+++ src/Maui.Sample/MauiProgram.cs
@@ -16,7 +16,7 @@ namespace Maui.Sample
                 });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
